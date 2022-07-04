6ac6210b908e668e71ee1a25ed0c8f4838e35117
Vivien <1494277+vlemoine@users.noreply.github.com>
2022-07-04 14:18:22 -0400

    Update app.vue

--- app.vue
+++ app.vue
@@ -1,9 +1,50 @@
 <template>
   <NuxtLayout>
+    <Html lang="en"></Html>
+    <Head><Title>calendar.rocks</Title></Head>
     <Body class="dark:bg-gray-900 text-black/80 dark:text-white"></Body>
+    <h1 class="sr-only">{{ months[selectedMonth] }} {{ selectedYear }}</h1>
     <!-- Color mode: {{ $colorMode.value }} -->
-    <div class="print:hidden flex items-center">
-      <font-awesome-icon icon="fa-solid fa-gear" />
+    <div id="Toolbar" class="print:hidden flex items-center mb-2 -mt-2 gap-2">
+      <div class="mr-auto flex gap-2">
+        <Button
+          @click="prevMonth()"
+          aria-label="Previous Month"
+          title="Previous Month"
+          class="aspect-square h-8"
+          ><font-awesome-icon icon="fa-solid fa-angle-left"
+        /></Button>
+        <Button @click="goToToday()"
+          ><font-awesome-icon icon="fa-solid fa-calendar-day" /> Today</Button
+        >
+        <Button
+          @click="nextMonth()"
+          aria-label="Next Month"
+          title="Next Month"
+          class="aspect-square h-8"
+          ><font-awesome-icon icon="fa-solid fa-angle-right"
+        /></Button>
+      </div>
+      <Button
+        @click="options = !options"
+        aria-label="Calendar options"
+        title="Calendar options"
+        :class="{ '!bg-indigo-100 dark:!bg-indigo-900': options }"
+        ><font-awesome-icon icon="fa-solid fa-gear"
+      /></Button>
+      <Button
+        aria-label="Print"
+        title="Print"
+        @click="print()"
+        class="!bg-lime-400 dark:!bg-lime-600 aspect-square h-16 rounded-full fixed right-3 bottom-3 z-50 shadow-lg !p-0 md:static md:h-12"
+        ><font-awesome-icon icon="fa-solid fa-print"
+      /></Button>
+    </div>
+    <div
+      id="Options"
+      class="print:hidden bg-indigo-500/20 p-4 rounded mb-2 flex gap-2 items-center"
+      :class="{ hidden: !options }"
+    >
       <input
         type="checkbox"
         name="allDates"
@@ -22,16 +63,16 @@
         id="forceFive"
         v-model="forceFive"
       /><label for="forceFive">Force five week view</label>
-      <select name="monthPos" v-model="monthPos" class="bg-transparent">
+      <select
+        name="monthPos"
+        v-model="monthPos"
+        class="bg-transparent"
+        aria-label="Month position"
+      >
         <option v-for="(op, i) in monthPosOptions" :key="i" :value="op">
           {{ op }}
         </option>
       </select>
-      <div class="ml-auto flex gap-2">
-        <Button @click="prevMonth()"><font-awesome-icon icon="fa-solid fa-angle-left" /></Button>
-        <Button @click="goToToday()"><font-awesome-icon icon="fa-solid fa-calendar-day" /> Today</Button>
-        <Button @click="nextMonth()"><font-awesome-icon icon="fa-solid fa-angle-right" /></Button>
-      </div>
     </div>
     <Month
       :date="date"
@@ -40,18 +81,25 @@
       :forceFive="forceFive"
       :monthPos="monthPos"
     >
-    <select name="months" v-model="selectedMonth" class="bg-transparent font-bold print:hidden w-fit">
+      <select
+        name="months"
+        v-model="selectedMonth"
+        class="bg-transparent font-bold print:hidden border border-black/20 dark:border-white/20 rounded h-[1.5em]"
+        aria-label="Month"
+      >
         <option v-for="(month, i) in months" :key="i" :value="i">
           {{ month }}
         </option>
       </select>
       <input
+        aria-label="Year"
         type="number"
         min="1900"
         v-model="selectedYear"
-        class="bg-transparent print:hidden w-[5ch]"
+        class="bg-transparent print:hidden w-[6ch] border border-black/20 dark:border-white/20 rounded h-[1.5em] px-[.25ch]"
       />
     </Month>
+    <div class="p-3 print:hidden"></div>
   </NuxtLayout>
 </template>
 
@@ -78,6 +126,7 @@ const allDates = ref(false);
 const abbrDay = ref(false);
 const forceFive = ref(false);
 const monthPos = ref("center");
+const options = ref(false);
 const date = computed(() => {
   let d = new Date();
   d.setDate(1);
@@ -99,6 +148,9 @@ function prevMonth() {
 function goToToday() {
   selectedMonth.value = new Date().getMonth();
   selectedYear.value = new Date().getFullYear();
+}
+function print() {
+  window.print();
 }
 </script>
 
