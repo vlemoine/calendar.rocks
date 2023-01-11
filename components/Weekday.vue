ccf9187e5cefd3c63a2c639a40bf8be1b1212231
Vivien <vivien@vivienlemoine.com>
2023-01-11 17:38:15 -0500

    Create Weekday.vue

---
+++ components/Weekday.vue
@@ -0,0 +1,34 @@
+<template>
+  <div
+    v-for="(weekday, i) in weekdays"
+    :key="i"
+    class="grid place-items-center"
+    :class="border"
+  >
+    <span
+      :class="{ 'md:hidden print:hidden': !abbrDay, 'print:inline': abbrDay }"
+      >{{ weekday.slice(0, 3) }}</span
+    >
+    <span
+      class="hidden md:inline"
+      :class="{ 'md:hidden print:hidden': abbrDay, 'print:inline': !abbrDay }"
+      >{{ weekday }}</span
+    >
+  </div>
+</template>
+
+<script setup lang="ts">
+const border: string = "border border-current";
+const weekdays: string[] = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+defineProps<{
+  abbrDay: boolean;
+}>();
+</script>
