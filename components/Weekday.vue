fa0cf8ab000e660e242177dacac57fca5afd7979
Vivien <vivien@vivienlemoine.com>
2023-01-11 19:33:58 -0500

    Update Weekday.vue
    - update abbreviation logic
    - move sunday to last if monday option enabled

--- components/Weekday.vue
+++ components/Weekday.vue
@@ -1,23 +1,22 @@
 <template>
-  <div
-    v-for="(weekday, i) in weekdays"
-    :key="i"
-    class="grid place-items-center"
-    :class="border"
-  >
-    <span
-      :class="{ 'md:hidden print:hidden': !abbrDay, 'print:inline': abbrDay }"
-      >{{ weekday.slice(0, 3) }}</span
+  <div class="col-span-7 grid grid-cols-7">
+    <div
+      v-for="(weekday, i) in weekdays"
+      :key="i"
+      class="grid place-items-center order-0"
+      :class="[border, {'order-last': last(weekday)}]"
     >
-    <span
-      class="hidden md:inline"
-      :class="{ 'md:hidden print:hidden': abbrDay, 'print:inline': !abbrDay }"
-      >{{ weekday }}</span
-    >
+      <span class="md:hidden print:hidden">{{ display(weekday, true) }}</span>
+      <span class="hidden md:inline print:inline">{{ display(weekday) }}</span>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
+const props = defineProps<{
+  abbrDay: boolean;
+  startMonday: boolean;
+}>();
 const border: string = "border border-current";
 const weekdays: string[] = [
   "Sunday",
@@ -28,7 +27,13 @@ const weekdays: string[] = [
   "Friday",
   "Saturday",
 ];
-defineProps<{
-  abbrDay: boolean;
-}>();
+const last = (weekday: string) => {
+  let wdi = weekdays.indexOf(weekday);
+  wdi = props.startMonday ? (wdi === 0 ? 6 : wdi - 1) : wdi;
+  return wdi === 6;
+};
+const display = (weekday: string, force: boolean = false) => {
+  const abbr = weekday.slice(0, 3);
+  return force ? abbr : props.abbrDay ? abbr : weekday;
+};
 </script>
